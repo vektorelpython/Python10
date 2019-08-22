6fd5224bed80f620b978e5ece42702505350bfce
İbrahim EDİZ <KURS>
2019-08-22 21:18:43 +0300

    22082019_2

--- 21_Fonksiyonlar6.py
+++ 21_Fonksiyonlar6.py
@@ -13,12 +13,105 @@ def Listele():
         adi,soyadi,telefon = liste[i].split(";")
         metin = "{}-{} {} {}".format((i+1),adi,soyadi,telefon)
         print(metin)
-dosya = open(r"deneme.csv","r+")
-print(dosya.read(18))
-print(dosya.tell())
-print(dosya.readline())
-print(dosya.readline())
-print(dosya.readline())
-dosya.seek(0)
-liste = dosya.readlines()
-print(liste)
+    dosya.close()
+
+
+def girisAl(**kwargs):
+    sonuc = ""
+    for key,value in kwargs.items():
+        if key=="ALANLAR":
+            for item in value:
+                sonuc+= input(item + " giriniz ") + ";"
+    sonuc = sonuc.rstrip(";") + "\n"
+    return sonuc 
+
+# def ekleme():
+#     kayit = girisAl(ALANLAR=["Adı","Soyadı","Telefon"])
+#     dosya = dosyaAc("deneme.csv")
+#     liste =  dosya.readlines()
+#     liste.insert(0,kayit)
+#     dosya.seek(0)
+#     dosya.truncate()
+#     dosya.writelines(liste)
+#     dosya.close()
+
+
+def arama():
+    pass
+
+# def silme():
+#     Listele()
+#     kayitID = int(input("Düzenlenecek Kaydı Seç"))
+#     dosya = dosyaAc("deneme.csv")
+#     liste = dosya.readlines()
+#     liste.pop(kayitID-1)
+#     dosya.seek(0)
+#     dosya.truncate()
+#     dosya.writelines(liste)
+#     dosya.close()
+#     Listele()
+
+# def duzeltme():
+#     Listele()
+#     
+#     dosya = dosyaAc("deneme.csv")
+#     liste = dosya.readlines()
+#     kayit = girisAl(ALANLAR=["Adı","Soyadı","Telefon"])
+#     liste[kayitID-1] = kayit
+#     dosya.seek(0)
+#     dosya.truncate()
+#     dosya.writelines(liste)
+#     dosya.close()
+
+
+def idu(param=0):
+    Listele()
+    kayit = ""
+    dosya = dosyaAc("deneme.csv")
+    liste = dosya.readlines()
+    # ekleme
+    if param == 1:
+        kayit = girisAl(ALANLAR=["Adı","Soyadı","Telefon"])
+        liste.insert(0,kayit)
+    #düzeltme
+    elif param == 2:
+        kayitID = int(input("Düzenlenecek Kaydı Seç"))
+        kayit = girisAl(ALANLAR=["Adı","Soyadı","Telefon"])
+        liste[kayitID-1] = kayit
+    #silme
+    elif param==3:
+        kayitID = int(input("Silinecek Kaydı Seç"))
+        liste.pop(kayitID-1)
+    dosya.seek(0)
+    dosya.truncate()
+    dosya.writelines(liste)
+    dosya.close()
+
+
+menu = """
+1-Ekleme
+2-Düzeltme
+3-Silme
+4-Listeleme
+5-Arama
+6-Çıkış
+"""
+anahtar = 1
+while anahtar == 1:
+    print("*"*30)
+    print(menu)
+    print("*"*30)
+    islem = int(input("İşlem Seçimi"))
+    if islem in (1,2,3):
+        idu(islem)
+    elif islem == 4:
+        Listele()
+    elif islem == 5:
+        arama()
+    elif islem == 6:
+        anahtar = 0
+    else:
+        print("Menü Dışında Seçenek Girdin ")
+else:
+    print("İyi Günler Dileriz")
+    
