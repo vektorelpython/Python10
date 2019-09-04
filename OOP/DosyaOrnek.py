647203a485663714735aacca8c3f7ff8b08ea3cb
İbrahim EDİZ <KURS>
2019-09-04 20:07:00 +0200

    otelotomasyondosyalı_2

--- OOP/DosyaOrnek.py
+++ OOP/DosyaOrnek.py
@@ -7,7 +7,7 @@ class OtelMusteri():
         self.musTel = ""
         self.Oda = ""
         self.GunSay = ""
-        self.Bas = dt.date()
+        self.Bas ="111"
         for key,value in kwargs.items():
             if key == "Ad":
                 self.musAd = value
@@ -20,6 +20,17 @@ class OtelMusteri():
                 self.odaBilgi = Dt(adres="odaBilgi",ALANLAR=["Kat","OdaNumara","DoluBos"])
             elif key == "GunSay":
                 self.GunSay = value
+        self.kayitOlustur()
 
+    def kayitOlustur(self):
+        self.MusKayit = Dt(adres="musteri",ALANLAR=["Adı","Soyadı","Tel","Oda","GunSay","Zaman"])
+        self.MusKayit.iduComp(self.musAd,self.musSoyad,self.musTel,self.Oda,self.GunSay,self.Bas,param=1)
+        self.odaGuncelle()
 
-    def odaGuncelle
+    def odaGuncelle(self,doluBos = "DOLU"):
+        kat = self.Oda[0] + "_KAT"
+        id = ((int(self.Oda[0])-1)*5) + int(self.Oda[2])
+        oda = self.Oda
+        self.odaBilgi.iduComp(kat,oda,doluBos,param=2,ID=id)
+
+deneme = OtelMusteri(Ad="İbrahim",Soyad="Ediz",Tel="05052643514",Oda="204",GunSay="5")
