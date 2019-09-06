0cec8d3d87bc2b2c99c43a9b16b8d75369129a3f
İbrahim EDİZ <KURS>
2019-09-06 19:01:06 +0200

    06092019_2

--- OOP/tkinterOrnek.py
+++ OOP/tkinterOrnek.py
@@ -1,21 +1,43 @@
+# import tkinter as tk
+# import time
+# pencere = tk.Tk()
+
+# def cikis():
+#     etiket["text"] = "Görüşmek Üzere"
+#     dugme["text"] = "Bekleyiniz"
+#     dugme["state"] = "disable"
+#     pencere.after(2000,pencere.destroy)
+
+# pencere.geometry('200x300')
+# etiket = tk.Label(text="Merhaba Vektorel Python")
+# etiket.pack()
+
+# dugme = tk.Button(text="Çıkış",command=cikis)
+# dugme.pack()
+
+# pencere.protocol("WM_DELETE_WINDOW",cikis)
+
+
+# pencere.mainloop()
+
+
 import tkinter as tk
-import time
-pencere = tk.Tk()
 
-def cikis():
-    etiket["text"] = "Görüşmek Üzere"
-    dugme["text"] = "Bekleyiniz"
-    dugme["state"] = "disable"
-    pencere.after(2000,pencere.destroy)
+class Pencere(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.protocol("WM_DELETE_WINDOW",self.cikis)
+        self.etiket = tk.Label(text="Merhaba Vektorel Python")
+        self.etiket.pack()
 
-pencere.geometry('200x300')
-etiket = tk.Label(text="Merhaba Vektorel Python")
-etiket.pack()
+        self.dugme = tk.Button(text="Çıkış",command=self.cikis)
+        self.dugme.pack()
+    
+    def cikis(self):
+        self.etiket["text"] = "Görüşmek Üzere"
+        self.dugme["text"] = "Bekleyiniz"
+        self.dugme["state"] = "disable"
+        self.after(2000,self.destroy)
 
-dugme = tk.Button(text="Çıkış",command=cikis)
-dugme.pack()
-
-pencere.protocol("WM_DELETE_WINDOW",cikis)
-
-
+pencere = Pencere()
 pencere.mainloop()
